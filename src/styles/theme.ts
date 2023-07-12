4eecd26a5c5ddd0fa0bfb2e8414338636bc7c716
Izadora <izadorasantosdesousa@gmail.com>
2023-07-12 10:06:40 -0300

    devfinder

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const  ligthTheme = {
+    body: 'rgb(221, 221, 249)',
+    fontColor: '#000',
+    searchAndCardBackground: '#fff',
+    inputPlaceholder: '#000',
+    MoreInformationsBackground: 'rgb(221, 221, 249)'
+   
+}
+
+export const  darkTheme = {
+    body: '#02122d',
+    fontColor: '#fff',
+    searchAndCardBackground: '#2c6ddc40',
+    inputPlaceholder: '#fff',
+    MoreInformationsBackground: '#1d1c1c'
+}
